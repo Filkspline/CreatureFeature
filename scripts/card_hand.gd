fddda57584d09a045fc972df2b99a345e4833e18
Braden-T-Thomas <thomasbrad1@myvuw.ac.nz>
2026-07-29 17:34:11 +1200

    Started working on controller input for the card ui as well as keyboard input
    Currently got the selection and stuff done, need to test with a controller proper

--- scripts/card_hand.gd
+++ scripts/card_hand.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 const UPGRADE_CARD = preload("res://scenes/upgrade_card.tscn")
 
-@export var hand_limit : int
+@export var hand_limit : int = 5 # Default 5
 @export var hand_width : float = 200 # Measured in pixels 200 is good for 5 cards
 @export var spread_curve: Curve
 @export var height_curve: Curve
@@ -14,13 +14,13 @@ var current_z_index : int
 var card_default_transform : Transform2D
 var card_default_rotation : float
 var defaults_set : bool
+var selected_card_idx : int
 
 ##------------------------------------------------------------------------
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_draw_hand()
-
 
 func _draw_hand() -> void:
 	current_z_index = card_default_z_index
@@ -53,17 +53,74 @@ func _spread_cards() -> void:
 		tween.tween_property(card, "transform", destination, 0.4)
 		tween.parallel().tween_property(card, "rotation", rotation_curve.sample(hand_ratio) * -0.3, 0.4)
 		await get_tree().create_timer(0.5).timeout
+	
+	hand.get_child(0).currently_highlighted = true
+	hand.get_child(0)._handle_highlight()
+	selected_card_idx = 0
+	
+func _input(event: InputEvent) -> void:
+	# Handles the input for the ui menu, god help us all
+	if event is InputEventMouseButton or event is InputEventMouseMotion:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	elif event is InputEventKey:
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+		if event.keycode == KEY_A and event.pressed:
+			if selected_card_idx == 0:
+				pass
+			else:
+				selected_card_idx -= 1
+				hand.get_child(selected_card_idx).currently_highlighted = true
+				hand.get_child(selected_card_idx)._handle_highlight()
+		elif event.keycode == KEY_D and event.pressed:
+			if selected_card_idx == (hand_limit - 1):
+				pass
+			else:
+				selected_card_idx += 1
+				hand.get_child(selected_card_idx).currently_highlighted = true
+				hand.get_child(selected_card_idx)._handle_highlight()
+
+	elif event is InputEventJoypadButton:
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+		if event.button_index == JOY_BUTTON_DPAD_LEFT and event.pressed:
+			if selected_card_idx == 0:
+				pass
+			else:
+				selected_card_idx -= 1
+				hand.get_child(selected_card_idx).currently_highlighted = true
+				hand.get_child(selected_card_idx)._handle_highlight()
+		elif event.button_index == JOY_BUTTON_DPAD_RIGHT and event.pressed:
+			if selected_card_idx == (hand_limit - 1):
+				pass
+			else:
+				selected_card_idx += 1
+				hand.get_child(selected_card_idx).currently_highlighted = true
+				hand.get_child(selected_card_idx)._handle_highlight()
+
+	elif event is InputEventJoypadMotion:
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		pass 
+
+	if event is InputEventJoypadButton:
+		if event.button_index == JOY_BUTTON_A:
+			_handle_clicked_card()
+	elif event is InputEventKey:
+		if event.keycode == KEY_ENTER or event.keycode == KEY_SPACE:
+			_handle_clicked_card()
 
 
-func _handle_clicked_card(clicked_card : Node2D):
+func _handle_clicked_card():
+	var highlighted_card : Node2D
 	for card in hand.get_children():
-		if card != clicked_card:
+		if card.currently_highlighted == false:
 			hand.remove_child(card)
 			card.queue_free()
-	
+		else:
+			highlighted_card = card
 	# Handles moving the selected card to the center of the screen,
 	# can be changed to move to a specific node down the line
 	var tween = create_tween()
-	tween.tween_property(clicked_card, "transform", card_default_transform, 0.4)
-	tween.parallel().tween_property(clicked_card, "rotation", card_default_rotation, 0.4)
+	tween.tween_property(highlighted_card, "transform", card_default_transform, 0.4)
+	tween.parallel().tween_property(highlighted_card, "rotation", card_default_rotation, 0.4)
 	# TODO to handle vfx if wanted or any other processes do so after the above code
--- scripts/upgrade_card.gd
+++ scripts/upgrade_card.gd
@@ -3,20 +3,30 @@ extends Node2D
 @onready var selection_icon = $Control/selection_icon
 @onready var parent_node = self.get_parent()
 
+var currently_highlighted : bool
 
 func _ready() -> void:
 	selection_icon.hide()
 
+
 func _on_texture_rect_mouse_entered() -> void:
-	selection_icon.show()
-	
+	currently_highlighted = true
+	_handle_highlight()
 
-func _on_texture_rect_mouse_exited() -> void:
-	selection_icon.hide()
- 
 
+func _handle_highlight() -> void:
+	if  currently_highlighted:
+		for card in self.get_parent().get_children():
+			if card != self:
+				card.currently_highlighted = false
+				card.selection_icon.hide()
+		selection_icon.show()
+	else:
+		selection_icon.hide()
+
+# TODO change to handle being the currently selected card
 func _on_texture_rect_gui_input(event: InputEvent) -> void:
 	# TODO This will need to be edited to handle controller input eventually
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			parent_node._handle_clicked_card(self)
+			parent_node._handle_clicked_card()
