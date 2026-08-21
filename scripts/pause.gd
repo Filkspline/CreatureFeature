b46c761b1608484b5800a6ab0009abe86a803e0e
brownchar7 <brownchar7@myvuw.ac.nz>
2026-08-21 20:22:16 +1200

    Controller/keyboard support

--- scripts/pause.gd
+++ scripts/pause.gd
@@ -1,23 +1,71 @@
 extends Control
-# BUGS: Not visible in editor? Not huge issue tho
+
+@onready var resume_button = $PanelContainer/VBoxContainer/Resume
+@onready var quit_button = $PanelContainer/VBoxContainer/Quit
+
+var buttons: Array[Button]
+var selected_index := 0
+
+
 func _ready():
+	buttons = [resume_button, quit_button]
+	
 	hide()
 	$AnimationPlayer.play("RESET")
+
 
 func resume():
 	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	hide()
+
 
 func pause():
 	show()
 	get_tree().paused = true
+	
+	# Start with Resume selected
+	selected_index = 0
+	buttons[selected_index].grab_focus()
+	
 	$AnimationPlayer.play("blur")
 
+
 func testEsc():
-	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
-		pause()
-	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
-		resume()
+	if Input.is_action_just_pressed("pause"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+
+
+func _process(_delta):
+	testEsc()
+	
+	# Controller / keyboard menu movement
+	if not get_tree().paused:
+		return
+	
+	if Input.is_action_just_pressed("MenuDown"):
+		selected_index += 1
+		
+		if selected_index >= buttons.size():
+			selected_index = 0
+		
+		buttons[selected_index].grab_focus()
+
+
+	if Input.is_action_just_pressed("MenuUp"):
+		selected_index -= 1
+		
+		if selected_index < 0:
+			selected_index = buttons.size() - 1
+		
+		buttons[selected_index].grab_focus()
+
+
+	if Input.is_action_just_pressed("MenuSelect"):
+		buttons[selected_index].pressed.emit()
 
 
 func _on_resume_pressed() -> void:
@@ -26,7 +74,4 @@ func _on_resume_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-
-func _process(delta):
-	testEsc()
+	SceneTransition.change_scene("res://scenes/main_menu.tscn")
