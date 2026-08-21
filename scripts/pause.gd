b00bc7104a84fc2f333fab827f922a6f2d8375e9
brownchar7 <brownchar7@myvuw.ac.nz>
2026-08-21 18:39:34 +1200

    Added Pause, broken

---
+++ scripts/pause.gd
@@ -0,0 +1,32 @@
+extends Control
+# Ive created bugs i didnt even know possible
+# Not visible in editor, also works here but when added to level buttons dont work
+func _ready():
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testEsc():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+# Breaks main menu when this is clicked? 
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+func _process(delta):
+	testEsc()
+
+# Ive made pause menus with this exact script and layout before, never had this happen
